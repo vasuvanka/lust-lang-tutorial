9e74d1eeefa0bdd505c4b0b28f2b217f2ff4d341
Vasu Vanka <vanka.vasu@gmail.com>
2023-09-30 12:38:07 +0530

    added print statements

---
+++ file_ops/src/main.rs
@@ -0,0 +1,34 @@
+use std::{fs, io::ErrorKind};
+
+fn main() {
+    let s = read_dir("src".to_owned());
+    for path in s {
+        let file_data = read_file(path);
+        println!("{:?}", file_data);
+    }
+}
+
+fn read_dir(file_path: String) -> Vec<String> {
+    let paths = fs::read_dir(file_path).unwrap();
+    let mut my_vec: Vec<String> = Vec::new();
+    for path in paths {
+        my_vec.push(path.unwrap().path().display().to_string());
+    }
+    return my_vec;
+}
+
+fn read_file(file_path: String) -> Vec<String> {
+    let files = fs::read_to_string(file_path);
+    let mut my_vec: Vec<String> = Vec::new();
+    let result = match files {
+        Ok(files) => files,
+        Err(e) if e.kind() == ErrorKind::NotFound => panic!("file not found : {}", e),
+        Err(e) if e.kind() == ErrorKind::PermissionDenied => panic!("permission denied : {}", e),
+        Err(e) => panic!("unknown error : {}", e),
+    };
+
+    result
+        .lines()
+        .for_each(|line| my_vec.push(line.to_string()));
+    return my_vec;
+}
--- hello_world/hello_world.rs
+++ hello_world/hello_world.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("hello world!")
+
+    println!("{number:>5}", number = 1);
+    println!("{number:0>5}", number = 1);
+
+    println!("{number:0>width$}", number = 1, width = 5);
 }
